47ac29b3c535ae1369a50b956174c5fd9001af7a
fiyyahzzhr <fiyyah.zahro@gmail.com>
2024-03-05 08:03:01 +0800

    Commit New File

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Proyek-Analisis-Data-Bike-Sharing-Data
+
+## Setup Environment
+conda create --name main-ds python=3.11
+conda activate main-ds
+pip install numpy pandas matplotlib seaborn jupyter streamlit babel
+
+## Run Streamlit App
+streamlit run dashboard.py
---
+++ dashboard/dashboard.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+
+with st.sidebar:
+    
+    st.image('download.jpeg')
+    st.title('Rental Bike')
+
+
+st.header('Dicoding Rental Bike')
+
+#main_df = pd.read_csv('main_data.csv')
+day_df = pd.read_csv('day.csv')
+hour_df = pd.read_csv('hour.csv')
+
+
+st.subheader('Penyewaan Sepeda Per Hari')
+
+
+daily_rentals = day_df.groupby(by="weekday").agg({"cnt": "sum"})
+
+
+st.set_option('deprecation.showPyplotGlobalUse', False) 
+st.bar_chart(daily_rentals)
+
+
+st.subheader('Penyewa Sepeda Per Jam')
+
+hourly_rentals = hour_df.groupby(by="hr").agg({"cnt": "sum"})
+
+st.set_option('deprecation.showPyplotGlobalUse', False) 
+st.line_chart(hourly_rentals)
+
+
+st.subheader('Perbandingan Penyewa Casual dan Registered Per Bulan')
+
+
+total_per_month = day_df.groupby(by="mnth").agg({
+    "cnt": "sum",
+    "casual": "sum",
+    "registered": "sum"
+})
+
+
+st.set_option('deprecation.showPyplotGlobalUse', False)  
+fig, ax = plt.subplots(figsize=(10, 6))
+
+
+month = total_per_month.index
+casual_rentals = total_per_month['casual']
+registered_rentals = total_per_month['registered']
+
+ax.bar(month, casual_rentals, width=0.4, label='Casual', color='skyblue')
+ax.bar(month + 0.4, registered_rentals, width=0.4, label='Registered', color='orange')
+
+ax.set_xticks(range(1, 13))  
+ax.set_xticklabels(['Jan', 'Feb', 'Mar', 'April', 'Mei', 'Juni', 'Juli', 'Agust', 'Sept', 'Okt', 'Nov', 'Des'])  
+ax.legend()
+
+st.pyplot(fig)
+    
